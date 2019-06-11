9176b84f376e1c7852a2fd9ac18504112a1adcbe
Jonathan Liu <jxliu95@gmail.com>
2019-06-11 16:56:53 +0000

    Added form validation (message length)

--- public/scripts/form-submission.js
+++ public/scripts/form-submission.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
+$(document).ready(() => {
   $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
-    $.ajax("/tweets/", {
-      method: "POST",
-      data: $(this).find("textarea").serialize(),
-      success: function() {
-        console.log("Successful POST");
-      }
-    });
+    if ($(this).find("textarea").val().length <= 0) {
+      alert("Please enter a message.");
+    } else if ($(this).find("textarea").val().length >= 140) {
+      alert("Your message is too long.");
+    } else {
+      $.ajax("/tweets/", {
+        method: "POST",
+        data: $(this).find("textarea").serialize(),
+        success: function() {
+          console.log("Successful POST");
+        }
+      });
+      $(this).find("textarea").val("");
+    }
   });
 });
